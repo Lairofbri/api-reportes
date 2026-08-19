5b79f97b103256c7561574d28a5dec6fea297091
lobrick <marroquinraul0729@gmail.com>
2026-08-18 18:20:16 -0600

    Update binaries and assembly information for ReportesAPI

--- src/ReportesAPI/obj/Debug/net10.0/ReportesAPI.AssemblyInfo.cs
+++ src/ReportesAPI/obj/Debug/net10.0/ReportesAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c93bc4d7ad3eee1e0e6920ea05ea0363e56e17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9924ba41a6796791b9b22f65ef04c72d12849ce9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
